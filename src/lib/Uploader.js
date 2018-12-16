3e6754c17c2eea1c4d55faac5fe65647881aa4ba
Piyush Bhangale <bhangalepiyush@gmail.com>
2018-12-16 08:41:48 +0530

    Add url

--- src/lib/Uploader.js
+++ src/lib/Uploader.js
@@ -7,17 +7,18 @@ module.exports = class Uploader{
      *
      * @param {string} token - API Token. Get yours from https://discordimages.com/
      */
-    constructor(token){
+    constructor(token, options = {}){
         if(!token) throw new Error("No Token Specified");
         if (typeof token !== 'string' || !token instanceof String) throw new TypeError("The Given param Isnt A String!")
         this.token = token;
+        this.url = options.url ? options.url : 'https://discordimages.com/api/upload';
         this.form = new FormData();
     };
 
     async upload(file){
         if(!isStream.readable(file)) throw new TypeError("Given File Is Not A Valid Readable Stream.");
         this.form.append('files[]',file);
-        let json = await fetch('https://discordimages.com/api/upload',{
+        let json = await fetch(this.url,{
             method: 'POST',
             body: this.form,
             headers: {
