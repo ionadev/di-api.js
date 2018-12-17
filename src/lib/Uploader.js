9455a576eb99830cd6629869a42c5b911abb3dd7
Your Name <bhangalepiyush@gmail.com>
2018-12-16 23:54:51 -0800

    Add user stats , bumb version

--- src/lib/Uploader.js
+++ src/lib/Uploader.js
@@ -15,11 +15,22 @@ module.exports = class Uploader{
     };
 
     async upload(file){
-        if(!isStream.readable(file)) throw new TypeError("Given File Is Not A Valid Readable Stream.");
+        //if(!isStream.readable(file)) throw new TypeError("Given File Is Not A Valid Readable Stream.");
         this.form.append('files[]',file);
         let json = await fetch('https://discordimages.com/api/upload',{
             method: 'POST',
             body: this.form,
+            headers: {
+                "token": this.token
+            }
+        }).then(res => res.json());
+
+        return json;
+    }
+
+    async  getUserStats(){
+        let json = await fetch('https://discordimages.com/api/storage/information',{
+            method: 'POST',
             headers: {
                 "token": this.token
             }
