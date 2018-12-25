5ae2e4f5d8c6c436d2d72226fef2520947f80ad1
Piyush Bhangale <bhangalepiyush@gmail.com>
2018-12-25 23:15:09 +0530

    ESM Released! 🎉 - Merry Christmas!

--- src/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    Uploader: require('./lib/Uploader'),
-    version: require('../package.json').version
-}
---
+++ src/index.mjs
@@ -0,0 +1,6 @@
+import package from '../package.json';
+
+export { default as Uploader } from './lib/Uploader';
+export const { version } = package;
+   
+
